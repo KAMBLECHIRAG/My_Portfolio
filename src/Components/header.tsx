63e2e17f25c1c5616543b23b399f469a6d6e59c5
Chirag Kamble <kamblechirag0@gmail.com>
2025-09-06 11:23:45 +0530

    removed error

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,7 +1,7 @@
 // src/components/Footer.tsx
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Header: React.FC = () => {
